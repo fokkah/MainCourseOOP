8f4672e6776a5eaddfa93329027ad9b3718402d8
fokkah <agameesport@gmail.com>
2025-01-27 16:28:15 +0100

    Made Person.class but still no error check

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,20 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Person erik = new Person("Erik", "Svensson", " erik@gmail.com ",  1);
+        Person anna = new Person("Anna ", "Olsson", " anna@gmail.com ",  2);
+
+        erik.setFirstName("Erik");
+        erik.setLastName("Svensson");
+        erik.setEmail("erik@gmail.com");
+        System.out.println("Person: " + erik.getSummary() );
+
+        anna.setFirstName("Anna");
+        anna.setLastName("Olsson");
+        anna.setEmail("anna@gmail.com");
+        System.out.println("Person: " + anna.getSummary() );
+
+
     }
 }
--- src/main/java/se/lexicon/Person.java
+++ src/main/java/se/lexicon/Person.java
@@ -1,4 +1,61 @@
 package se.lexicon;
 
 public class Person {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String eMail;
+
+    private String summary;
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String eMail) {
+        this.eMail = eMail;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public Person(String firstName, String lastName, String eMail, int id){
+        this.id = id;
+        this.firstName = firstName + "First name";
+        this.lastName = lastName + "Last name";
+        this.eMail = eMail + "e-Mail";
+        this.summary = "ID:"+id+ " Name:" + firstName+""+lastName+ "," + " e-Mail:" + eMail;
+
+
+    }
+
 }
